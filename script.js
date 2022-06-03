6f77870b8a6fdbd375b01ecafb0cad8590822777
Philip Lages <psilvaelages@gmail.com>
2022-06-03 20:07:22 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -4,23 +4,80 @@ function createNewTag(tag, element, value) {
   return createTag;
 }
 
-function createRandomColors(number) {
-  const red = Math.floor(Math.random() * number);
-  const green = Math.floor(Math.random() * number);
-  const blue = Math.floor(Math.random() * number);
-  return 'rgb' + `(${red}, ${green}, ${blue})`;
+const styles = ['newspaper', 'magazine1', 'magazine2'];
+
+const sizes = ['medium', 'big', 'reallybig'];
+
+const rotations = ['rotateleft', 'rotateright'];
+
+const inclinations = ['skewleft', 'skewright'];
+
+
+function chooseRandomStyle() {
+  const whichStyle = Math.floor(Math.random() * 3);
+  let chosenStyle;
+  for (let style = 0; style < styles.length; style += 1) {
+    chosenStyle = styles[whichStyle];
+  }
+  return chosenStyle;
+}
+
+function chooseRandomSize() {
+  const whichSize = Math.floor(Math.random() * 3);
+  let chosenSize;
+  for (let size = 0; size < sizes.length; size += 1) {
+    chosenSize = sizes[whichSize];
+  }
+  return chosenSize;
+}
+
+function chooseRandomRotation() {
+  const whichRotation = Math.floor(Math.random() * 2);
+  console.log(whichRotation);
+  let chosenRotation;
+  for (let rotation = 0; rotation < rotations.length; rotation += 1) {
+    chosenRotation = rotations[whichRotation];
+  }
+  console.log(chosenRotation);
+  return chosenRotation;
+}
+
+function chooseRandomInclination() {
+  const whichInclination = Math.floor(Math.random() * 2);
+  let chosenInclination;
+  for (let inclination = 0; inclination < inclinations.length; inclination += 1) {
+    chosenInclination = inclinations[whichInclination];
+  }
+  return chosenInclination;
 }
 
 const input = document.getElementById('carta-texto');
 const letter = document.getElementById('carta-gerada');
 const generateBtn = document.getElementById('criar-carta');
+const counter = document.getElementById('carta-contador');
 
 function generateLetter() {
-  console.log(letter.value);
-  if(letter.value === undefined) {    
-    letter.innerText = 'Por favor, digite o conteúdo da carta';
+  const classes = `${chooseRandomStyle()} ${chooseRandomSize()} ${chooseRandomRotation()} ${chooseRandomInclination()}`;
+
+  const words = input.value.split(' ');
+  counter.innerText = words.length;
+
+  console.log(letter);
+  for (let word = 0; word < words.length; word += 1) {  
+    let span = document.createElement('span');
+    span.className = classes;
+    letter.appendChild(span);
+    letter.lastElementChild.innerText = words[word];
+    console.log(classes);
   }
-  letter.innerText = input.value;  
 }
 
-generateBtn.addEventListener('click', generateLetter);
+function addBtnEvents() {
+  generateBtn.addEventListener('click', generateLetter);  
+}
+addBtnEvents();
+
+
+
+
+
